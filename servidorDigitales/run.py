f7cca33a8f54e666a32c76ca5e0161b539dfaeb0
Daniela Rojas <ddp.rojas11@uniandes.edu.co>
2020-06-16 18:05:36 -0500

    modificaciones para utilizar serial

--- servidorDigitales/run.py
+++ servidorDigitales/run.py
@@ -12,9 +12,50 @@ global MENSAJE_CONSOLA
 filename = " "
 MENSAJE_CONSOLA = ""
 
+# serial
+import serial
+global estadosSW
+estadosSW = [ '' , '']
+#Creamos los parametros para comunicacion serial
+DIRECCION_Serial = "/dev/tty.usbmodem14201"
+BAUD_RATE = 9600
+try:
+	serialFPGA = serial.Serial(DIRECCION_Serial, baudrate=BAUD_RATE)
+	print("Comunicacion Serial inicializado")
+except:
+	print("No fue posible iniciar el serial")
+#Iniciamos la comunicacion Serial
+def iniciarSerial():
+	try:
+		serialFPGA = serial.Serial(DIRECCION_Serial, baudrate=BAUD_RATE)
+		print("Comunicacion Serial inicializado")
+		return 1
+	except:
+		print("No fue posible iniciar el serial")
+		return 0
+
+# Funcion para enviar por serial
+def enviarPorSerial(mensaje):
+	serialFPGA.write( ( (str(mensaje)+'\n') ).encode() )
+
+# Funcion para apagar la camara
+def apagarCamara():
+	#COMANDO_SHUT_DOWN_STREAMING ='killall obs'
+	#ans = os.popen(COMANDO_SHUT_DOWN_STREAMING).read()
+	#print(ans)
+	print('apagar Camara')
+
+# Funcion para apagar la camara
+def prenderCamara():
+	print('prender')
+	#COMANDO_START_STREAMING ='obs --startstreaming'
+	#ans = os.popen(COMANDO_START_STREAMING).read()
+	#print(ans)
+
 @app.route("/")
 def index():
-		# renderiamos la plantilla "index.html"
+	apagarCamara()
+		# renderizamos la plantilla "index.html"
 	return render_template('index.html', filename = filename, MENSAJE_CONSOLA = MENSAJE_CONSOLA)
 
 #### Ruta para recibir archivo de projecto
@@ -26,7 +67,7 @@ def uploader():
  		f = request.files['archivo']
  		filename = secure_filename(f.filename)
  		# Eliminamos el archivo actual si existe
- 		if path.exists("./received_Files/BB_SYSTEM.sof"):
+ 		if os.path.exists("./received_Files/BB_SYSTEM.sof"):
  			os.remove('./received_Files/BB_SYSTEM.sof')
  		# Guardamos el archivo en el directorio "Archivos PDF"
  		f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
@@ -45,24 +86,34 @@ def verify():
 		#COMANDO_UPLOAD_CODE_FPGA = "quartus_pgm -m jtag -o 'p;./received_Files/BB_SYSTEM.sof"
 		MENSAJE_CONSOLA = os.popen(COMANDO_UPLOAD_CODE_FPGA).read()
 		print('MENSAJE_CONSOLA',MENSAJE_CONSOLA)
+		iniciarSerial()
 	return redirect("/")
 
 
 ### Ruta para recibir la información del switch Camara, para así comenzar a obtener video
 @app.route('/switchCamara', methods = ['POST'])
-def switchMLC():
+def switchC():
 	estadoCamaraNuevo = request.form['CAMARA']
 	print("la nueva accion de la CAMARA es : "  + estadoCamaraNuevo)
 	if(estadoCamaraNuevo=='true'):
-		print('prender')
-		#COMANDO_START_STREAMING ='obs --startstreaming'
-		#ans = os.popen(COMANDO_START_STREAMING).read()
-		#print(ans)
+		prenderCamara()
 	elif(estadoCamaraNuevo=='false'):
-		print('apagar')
-		#COMANDO_SHUT_DOWN_STREAMING ='killall obs'
-		#ans = os.popen(COMANDO_SHUT_DOWN_STREAMING).read()
-		#print(ans)
+		apagarCamara()
+	return ""
+
+
+### Ruta para recibir la información de los swithches para la FPGA
+@app.route('/switchFPGA', methods = ['POST'])
+def switchsSeñales():
+	global estadosSW
+	estadoSW1 = request.form['SW1']
+	if not estadoSW1 == '00':
+		estadosSW[0] = estadoSW1
+	estadoSW2 = request.form['SW2']
+	if not estadoSW2 == '00':
+		estadosSW[1] = estadoSW2
+	print(estadosSW)
+	enviarPorSerial(estadosSW)
 	return ""
 
 
